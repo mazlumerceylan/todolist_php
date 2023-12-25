b4328c0a4ac7635b1fbca32b2d81eb3024d9ecf4
Mazlum Erceylan <mazlumerceylan98@gmail.com>
2023-12-25 12:45:51 +0100

    processed task code

--- add_task.php
+++ add_task.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Todo List</title>
+    <title>Neue Aufgabe hinzufügen</title>
 </head>
 <body>
     <h2>Neue Aufgabe hinzufügen</h2>
@@ -11,9 +11,9 @@
         <label for="title">Titel:</label><br>
         <input type="text" id="title" name="title" required><br>
         <label for="description">Beschreibung:</label><br>
-        <textarea id="description" name="description"></textarea><br>
-        <label for="deadline">Frist:</label><br>
-        <input type="date" id="deadline" name="deadline"><br><br>
+        <textarea id="description" name="description" required></textarea><br>
+        <label for="due_date">Fälligkeitsdatum:</label><br>
+        <input type="date" id="due_date" name="due_date" required><br><br>
         <input type="submit" value="Aufgabe hinzufügen">
     </form>
 </body>
