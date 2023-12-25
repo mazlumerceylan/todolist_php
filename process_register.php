e7d21f9dd7b14f43a13db5bfefa97e99ba045461
Mazlum Erceylan <mazlumerceylan98@gmail.com>
2023-12-25 12:46:16 +0100

    processed register code

---
+++ process_register.php
@@ -0,0 +1,36 @@
+<?php
+include_once 'config.php'; // Stelle sicher, dass diese Zeile die Konfigurationsdatei für die Datenbankverbindung korrekt einbindet
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hash des Passworts für die Sicherheit
+
+    // Überprüfen, ob die E-Mail bereits in der Datenbank registriert ist
+    $sql_check = "SELECT id FROM users WHERE email = ? LIMIT 1";
+    $stmt_check = $conn->prepare($sql_check);
+    $stmt_check->bind_param('s', $email);
+    $stmt_check->execute();
+    $result_check = $stmt_check->get_result();
+
+    if ($result_check->num_rows == 0) {
+        // Wenn die E-Mail noch nicht registriert ist, fahre mit der Registrierung fort
+        $sql_register = "INSERT INTO users (first_name, last_name, email, password) VALUES (?, ?, ?, ?)";
+        $stmt_register = $conn->prepare($sql_register);
+        $stmt_register->bind_param('ssss', $first_name, $last_name, $email, $password);
+        if ($stmt_register->execute()) {
+            echo "Registrierung erfolgreich abgeschlossen. <a href='login.php'>Anmelden</a>";
+        } else {
+            echo "Fehler bei der Registrierung: " . $conn->error;
+        }
+        $stmt_register->close();
+    } else {
+        echo "Diese E-Mail ist bereits registriert.";
+    }
+    $stmt_check->close();
+} else {
+    header("Location: register.php"); // Wenn der Zugriff nicht über die POST-Methode erfolgt, leite um
+    exit();
+}
+?>
