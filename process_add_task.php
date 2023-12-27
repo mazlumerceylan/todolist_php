14f9d4f02c0e5e5976df51882276f0d4a2069f35
Mazlum Erceylan <mazlumerceylan98@gmail.com>
2023-12-27 17:59:03 +0100

    bug fixes

--- process_add_task.php
+++ process_add_task.php
@@ -24,3 +24,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_SESSION['user_id'])) {
     exit();
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="processed_tasks.css">
+</head>
+<body>
+    
+</body>
+</html>
